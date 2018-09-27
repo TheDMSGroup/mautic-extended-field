789a92bcc928b078916db4c176f834ed5d525118
scottshipman <k.scott.shipman@gmail.com>
2018-09-27 14:31:24 -0400

    php cs fixer

--- Entity/ExtendedFieldRepositoryTrait.php
+++ Entity/ExtendedFieldRepositoryTrait.php
@@ -250,12 +250,12 @@ trait ExtendedFieldRepositoryTrait
                             && 'boolean' == $extendedField['type']
                         )
 
-                        || (
+                        || $changes['fields'][$extendedField['alias']][1] !== (
                             // value contained extra space in text/string field detected as change when none really exist
                             is_string($changes['fields'][$extendedField['alias']][0]) && is_string($changes['fields'][$extendedField['alias']][1])
-                            && 
+                            &&
                             trim($changes['fields'][$extendedField['alias']][0]) === trim($changes['fields'][$extendedField['alias']][1])
-                            && $changes['fields'][$extendedField['alias']][0]) !== $changes['fields'][$extendedField['alias']][1]
+                            && $changes['fields'][$extendedField['alias']][0])
                         )
 
                         && null !== $changes['fields'][$extendedField['alias']][1]
